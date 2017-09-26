e76f5d6d8b2a73d24bdb80ffb828ce2e05cdb2c4
KaluginDV <interrupted-man@yandex.ru>
2017-09-26 22:14:15 +0800

    Исправлена ошибка, когда датчик выдает -127 градусов. Добавлены задержки при выводе на экран. Добавлено пожелание хорошего дня =) Добавлен счетчик (часы:минуты:секунды) времени работы устройства после подачи питания.

--- Podval_Project_19_06_2016/Podval_Project_19_06_2016.ino
+++ Podval_Project_19_06_2016/Podval_Project_19_06_2016.ino
@@ -1,7 +1,7 @@
 //На данный момент работают только два термпературных датчика
 //В планах добавить реле на включение провертривания помещения в зависимости от разницы тимператур
 //добавить третий уличный датчик температуры
-/ добавить датчик влажности для подвала
+// добавить датчик влажности для подвала
 // Подключаем библитеотеку для подлючения датчиков на одной шине
 #include <OneWire.h>
 // Подключаем библиотеку работы с температурными датчиками
@@ -37,17 +37,29 @@ void setup() {
   maxTemp1 = ds.getTempC(sensor1);
   minTemp2 = ds.getTempC(sensor2);
   maxTemp2 = ds.getTempC(sensor2);
-  lcd.setCursor(3, 0);
+  lcd.setCursor(2, 0);
   lcd.print(L"ГАРАЖНЫЙ");
-  lcd.setCursor(3, 1);
+
+  delay(100);
+
+  lcd.setCursor(4, 1);
   lcd.print(L"ТЕРМОМЕТР");
+
+  delay(5000);
+
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print(L"ХОРОШЕГО");
+  delay(100);
+  lcd.setCursor(4, 1);
+  lcd.print(L"ВАМ ДНЯ!");
+
+  delay(5000);
 
 }
 
 void loop() {
-  
-  delay(5000);
-  
+
   ds.requestTemperatures(); // считываем температуру с датчиков
 
   lcd.clear();
@@ -58,41 +70,50 @@ void loop() {
   sens1temp = ds.getTempC(sensor1); // отправляем температуру
   lcd.print(sens1temp);
   lcd.print("C");
+
+  delay(100);
+
   lcd.setCursor(0, 1);
   lcd.print(L"ПОДВАЛ");
   lcd.print(":  ");
   sens2temp = ds.getTempC(sensor2); // отправляем температуру
-  lcd.print(sens2temp);  
+  lcd.print(sens2temp);
   lcd.print("C");
 
-  if (sens1temp>=maxTemp1) {
+  if (sens1temp >= maxTemp1) {
     maxTemp1 = sens1temp;
   }
 
-  if (sens2temp>=maxTemp2) {
+  if (sens2temp >= maxTemp2) {
     maxTemp2 = sens2temp;
   }
 
-  if (sens1temp<minTemp1) {
+  if (sens1temp < minTemp1 && sens1temp > -127) {
     minTemp1 = sens1temp;
   }
 
-  if (sens2temp<minTemp2) {
+  if (sens2temp < minTemp2 && sens1temp > -127) {
     minTemp2 = sens2temp;
-  }  
+  }
 
   delay(10000);
-  
+
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(L"МАКСИМУМ");
+
+  delay(100);
+
   lcd.setCursor(0, 1);
   lcd.print(L"Г");
-  lcd.print(" ");  
+  lcd.print(":");
   lcd.print(maxTemp1);
   lcd.print(" ");
+
+  delay(100);
+
   lcd.print(L"П");
-  lcd.print(" ");  
+  lcd.print(":");
   lcd.print(maxTemp2);
 
   delay(5000);
@@ -100,13 +121,51 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(L"МИНИМУМ");
+
+  delay(100);
+
   lcd.setCursor(0, 1);
   lcd.print(L"Г");
-  lcd.print(" ");  
+  lcd.print(":");
   lcd.print(minTemp1);
   lcd.print(" ");
+
+  delay(100);
+
   lcd.print(L"П");
-  lcd.print(" ");  
+  lcd.print(":");
   lcd.print(minTemp2);
 
+  delay(5000);
+
+  Time();
+
+}
+
+//Подсчет времени наработки после включения
+void Time()
+{
+  for (int i = 0; i < 60; i++) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(L"ВРЕМЯ РАБОТЫ");
+    lcd.setCursor(0, 1);
+    int time = millis() / 1000;
+    if (time / 60 / 60 < 10) {
+      lcd.print ("0");
+    }
+    lcd.print (time / 60 / 60);
+    lcd.print (":");
+    if (time / 60 % 60 < 10) {
+      lcd.print ("0");
+    }
+    lcd.print ((time / 60) % 60);
+    lcd.print (":");
+    if (time % 60 < 10) {
+      lcd.print ("0");
+    }
+    lcd.print (time % 60);
+    delay(100);
+  }
+  //delay(3000);
 }
